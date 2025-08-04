c3c98cfef22e6912a2af3c290b2740524099ef1e
evandro <evandroguimaraesalves@gmail.com>
2025-08-04 14:14:06 -0300

    criar o model para Pizzaria

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Pizzaria(models.Model):
+    nome = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=14, unique=True)
+    endereco = models.TextField()
+    telefone = models.CharField(max_length=15)
+    ativa = models.BooleanField(default=True)
+    criada_em = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Pizzaria"
+        verbose_name_plural = "Pizzarias"
+        ordering = ['nome']
+    
+    def __str__(self):
+        return self.nome
+    
+    # Futuro: configurações específicas da pizzaria
+    # cor_tema, logo, configurações_delivery, etc.
