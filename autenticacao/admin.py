d1846a4bd68ea72dddb944038b342a53b2f36a22
evandro <evandroguimaraesalves@gmail.com>
2025-08-04 18:58:35 -0300

    na tela de admin agora eu posso criar usuarios para o sistema

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Pizzaria, UsuarioPizzaria
 
-# Register your models here.
+
+@admin.register(Pizzaria)
+class PizzariaAdmin(admin.ModelAdmin):
+    list_display = ("nome", "cnpj", "telefone", "ativa", "criada_em")
+    list_filter = ("ativa",)
+    search_fields = ("nome", "cnpj")
+
+
+@admin.register(UsuarioPizzaria)
+class UsuarioPizzariaAdmin(admin.ModelAdmin):
+    list_display = (
+        "usuario",
+        "pizzaria",
+        "papel",
+        "ativo",
+        "criado_em",
+    )
+    list_filter = ("papel", "ativo", "pizzaria")
+    search_fields = (
+        "usuario__username",
+        "usuario__email",
+        "pizzaria__nome",
+    )
